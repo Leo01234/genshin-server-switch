f1dca8eaa232a8b9db6e8da3a6c94b98508911a9
Leo01234 <ijkhly@163.com>
2022-10-01 19:58:37 +0800

    GUI基本完成

--- switch.py
+++ switch.py
@@ -1,13 +1,100 @@
 import ctypes
 import configparser
 import os
-import sys
+import shutil
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
 
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
+
+
+def report_callback_exception(self, exc, val, tb):
+    messagebox.showerror("Error", message=str(val))
+
+
+def check():
+    global launcher_config
+    global game_config
+
+    global launcher_config_status
+    global game_config_status
+    global PCGameSDK_status
+
+    global old
+
+    launcher_config = configparser.ConfigParser()
+    game_config = configparser.ConfigParser()
+
+    launcher_config.read(launcher_config_path)
+    game_config.read(game_config_path)
+
+    if mihoyo.items() <= launcher_config['launcher'].items():
+        launcher_config_status.set('官服')
+    elif bilibili.items() <= launcher_config['launcher'].items():
+        launcher_config_status.set('b服')
+    else:
+        launcher_config_status.set('状态异常')
+
+    if mihoyo.items() <= game_config['General'].items():
+        game_config_status.set('官服')
+    elif bilibili.items() <= game_config['General'].items():
+        game_config_status.set('b服')
+    else:
+        game_config_status.set('状态异常')
+
+    if not os.path.isfile(PCGameSDK_path):
+        PCGameSDK_status.set('官服')
+    elif os.path.isfile(PCGameSDK_path):
+        PCGameSDK_status.set('b服')
+    else:
+        PCGameSDK_status.set('状态异常')
+
+    if (launcher_config_status.get() == '官服' and
+            game_config_status.get() == '官服' and
+            PCGameSDK_status.get() == '官服'):
+        old = 'mihoyo'
+    elif (launcher_config_status.get() == 'b服' and
+          game_config_status.get() == 'b服' and
+          PCGameSDK_status.get() == 'b服'):
+        old = 'bilibili'
+    else:
+        old = 'error'
+        messagebox.showinfo(message="配置文件或 PCGameSDK.dll 文件异常, 请手动检查")
+
+
+def check_and_switch():
+    check()
+
+    if old == 'mihoyo':
+        launcher_config['launcher'].update(bilibili)
+        game_config['General'].update(bilibili)
+
+        shutil.copy(PCGameSDK_name, PCGameSDK_folder)
+        # os.system("copy " + PCGameSDK_name + ' "' + PCGameSDK_folder + '"')
+        if not os.path.isfile(PCGameSDK_path):
+            messagebox.showinfo(message="文件复制错误")
+
+    elif old == 'bilibili':
+        launcher_config['launcher'].update(mihoyo)
+        game_config['General'].update(mihoyo)
+
+        os.remove(PCGameSDK_path)
+        if os.path.isfile(PCGameSDK_path):
+            messagebox.showinfo(message="文件未正常删除")
+
+    with open(launcher_config_path, 'w') as configfile:
+        launcher_config.write(configfile)
+    with open(game_config_path, 'w') as configfile:
+        game_config.write(configfile)
+
+    check()
+
 
 if __name__ == "__main__":
     if is_admin():
@@ -19,61 +106,49 @@ if __name__ == "__main__":
 
         PCGameSDK_path = r'C:\Program Files\Genshin Impact\Genshin Impact Game\YuanShen_Data\Plugins\PCGameSDK.dll'
         PCGameSDK_folder, PCGameSDK_name = os.path.split(PCGameSDK_path)
-        
+
         launcher_config = configparser.ConfigParser()
         game_config = configparser.ConfigParser()
 
-        launcher_config.read(launcher_config_path)
-        game_config.read(game_config_path)
+        old = ''
 
-        # ============= 检查 ============
-        if (mihoyo.items() <= launcher_config['launcher'].items() and
-            mihoyo.items() <= game_config['General'].items() and
-            not os.path.isfile(PCGameSDK_path)):
-            # 现在是官服
-            old = 'mihoyo'
-            print("当前是官服")
+        # ============= GUI ============
+        # override method
+        Tk.report_callback_exception = report_callback_exception
 
-        elif (bilibili.items() <= launcher_config['launcher'].items() and
-            bilibili.items() <= game_config['General'].items() and
-            os.path.isfile(PCGameSDK_path)):
-            # 现在是b服
-            old = 'bilibili'
-            print("当前是b服")
+        root = Tk()
 
-        else:
-            print("配置文件或 PCGameSDK.dll 文件异常")
-            sys.exit()
+        launcher_config_status = StringVar()
+        game_config_status = StringVar()
+        PCGameSDK_status = StringVar()
 
-        # ============= 变更 ============
-        if old == 'mihoyo':
-            launcher_config['launcher'].update(bilibili)
-            game_config['General'].update(bilibili)
+        check()
 
-            os.system("copy " + PCGameSDK_name + ' "' + PCGameSDK_folder + '"')
-            if not os.path.isfile(PCGameSDK_path):
-                print("文件复制错误")
-                sys.exit()
+        root.title("官服b服转换")
 
-        elif old == 'bilibili':
+        mainframe = ttk.Frame(root, padding="3 3 3 3")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
 
-            launcher_config['launcher'].update(mihoyo)
-            game_config['General'].update(mihoyo)
+        ttk.Label(mainframe, text="启动器配置文件状态: ").grid(column=1, row=1, sticky=W)
+        ttk.Label(mainframe, text="游戏配置文件状态: ").grid(column=1, row=2, sticky=W)
+        ttk.Label(mainframe, text="PCGameSDK.dll 文件状态: ").grid(column=1, row=3, sticky=W)
 
-            os.remove(PCGameSDK_path)
-            if os.path.isfile(PCGameSDK_path):
-                print("文件未正常删除")
-                sys.exit()
+        ttk.Label(mainframe, textvariable=launcher_config_status).grid(column=2, row=1, sticky=E)
+        ttk.Label(mainframe, textvariable=game_config_status).grid(column=2, row=2, sticky=E)
+        ttk.Label(mainframe, textvariable=PCGameSDK_status).grid(column=2, row=3, sticky=E)
 
-        with open(launcher_config_path, 'w') as configfile:
-            launcher_config.write(configfile)
-        with open(game_config_path, 'w') as configfile:
-            game_config.write(configfile)
+        check_button = ttk.Button(mainframe, text="检查", command=check)
+        check_button.grid(column=1, row=4, sticky=W)
+        check_and_switch_button = ttk.Button(mainframe, text="检查并切换", command=check_and_switch)
+        check_and_switch_button.grid(column=2, row=4, sticky=E)
 
-        if old == 'mihoyo':
-            print("已从官服转为b服")
-        elif old == 'bilibili':
-            print("已从b服转为官服")
+        for child in mainframe.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+        check_and_switch_button.focus()
+
+        root.mainloop()
 
     else:
-        print("需要以管理员权限运行")
+        messagebox.showinfo(message="需要以管理员权限运行")
